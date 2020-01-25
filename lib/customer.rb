fb107a754e70d1d69b372d47b13f1e6c564604c6
Austin Johnson <technicallyaustin@gmail.com>
2020-01-25 22:10:44 +0000

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,21 @@
 class Customer
+  @@all = []
+  attr_accessor :name, :age, :waiter, :total, :tip, :meal 
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self 
+  end
+  
+  def new_meal(self, waiter,total, tip )
+    we
+  
+  
+  
+  
+  def self.all
+    @@all
+  end
+    
   
 end
